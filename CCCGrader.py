5736a34823c4188056d74bb1abbdf8f4562b3968
Hao Yun <Haoy2001@gmail.com>
2018-07-25 21:47:44 -0400

    Back button and frame switching implemented

--- CCCGrader.py
+++ CCCGrader.py
@@ -2,58 +2,69 @@ import tkinter as tk
 import pygubu
 import codeCompare
 
-women = object()
-
 class Application:
 
     #store objects
-    yearSelecter = object()
+    yearSelector = object()
+    probSelect = object()
+    testBench = object()
+    master = object()
 
     def __init__(self, master):
 
         self.builder = builder = pygubu.Builder()
+        self.master = master
 
         #load ui file
         builder.add_from_file('GUI.ui')
 
-        #create the widget using a master as parent
-        self.mainwindow = builder.get_object('probSelect', master)
-
-        #make 2018 default selection instead of 0
-        self.yearSelecter = builder.get_object('yearSelector', master)
-        self.yearSelecter.set(2018)
+        #store objects
+        self.probSelect = self.builder.get_object('probSelect', self.master)
+        self.testBench = self.builder.get_object('testBench', self.master)
+        self.yearSelector = self.builder.get_object('yearSelector', self.master)
         
-        #link buttons though callback
+        #show window
+        self.showSelector()
+
         callbacks = {
-            'J1': lambda: self.selection("J1"),
-            'J2': lambda: self.selection("J2"),
-            'J3': lambda: self.selection("J3"),
-            'J4': lambda: self.selection("J4"),
-            'J5': lambda: self.selection("J5"),
-            'S1': lambda: self.selection("S1"),
-            'S2': lambda: self.selection("S2"),
-            'S3': lambda: self.selection("S3"),
-            'S4': lambda: self.selection("S4"),
-            'S5': lambda: self.selection("S5"),
-
+        'J1': lambda: self.selectionMade("J1"),
+        'J2': lambda: self.selectionMade("J2"),
+        'J3': lambda: self.selectionMade("J3"),
+        'J4': lambda: self.selectionMade("J4"),
+        'J5': lambda: self.selectionMade("J5"),
+        'S1': lambda: self.selectionMade("S1"),
+        'S2': lambda: self.selectionMade("S2"),
+        'S3': lambda: self.selectionMade("S3"),
+        'S4': lambda: self.selectionMade("S4"),
+        'S5': lambda: self.selectionMade("S5"),
+        'back': lambda: self.showSelector()
         }
-        builder.connect_callbacks(callbacks)
+        self.builder.connect_callbacks(callbacks)
 
-    #handle button click
-    def selection(self, name):
-        print(self.yearSelecter.get() + " " + name)
-        # print(selected)
+    #show selection window
+    def showSelector(self):
+        self.probSelect.tkraise() 
+        self.master.title('CCCCCC - Select Question')
+        #make 2018 default selection instead of 0
+        self.yearSelector.set(2018)
 
+    #question has been selected
+    def selectionMade(self, question):
+        self.testBench.tkraise() 
+        yearSelected = self.yearSelector.get()
+        questionSelected = question
+        self.master.title("CCCCCC - " +  str(yearSelected) + " " + question)
 
-if __name__ == '__main__':
-    
-    root = tk.Tk()
-    app = Application(root)
+yearSelected = 2018
+questionSelected = "J1"
 
-    #app settings
-    root.resizable(False, False)
-    root.title('CCCCCC - Select Question')
+root = tk.Tk()
+application = Application(root)
 
-    codeCompare.test("2018q3sol.exe", "windows_data\S3\s3.3-02.in", b'temp')
+#app settings
+root.resizable(False, False)
+root.title('CCCCCC - Select Quesoaeaotion')
 
-    root.mainloop()
+#codeCompare.test("2018q3sol.exe", "windows_data\S3\s3.3-02.in", b'temp')
+
+root.mainloop()
