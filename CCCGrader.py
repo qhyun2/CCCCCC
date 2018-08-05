8605738c7318a5cbf5964b01c4378a9489a8c8eb
Hao Yun <Haoy2001@gmail.com>
2018-08-04 20:41:12 -0400

    Test Case Selection Interface Added

--- CCCGrader.py
+++ CCCGrader.py
@@ -1,7 +1,57 @@
 import tkinter as tk
 from tkinter.font import Font
 import pygubu
+import glob
+import os
+
 import codeCompare
+
+
+class testCaseManager:
+
+    container = object()
+    checkboxList = []
+    varList={}
+
+    def __init__(self, filelist, container):
+        self.container = container
+        container.delete(1.0, tk.END)
+        for i in filelist:
+            self.addCheckbox(i)
+        self.selectAll()
+    
+    def addCheckbox(self, filename):
+        newCheckbox = tk.Checkbutton(master=self.container)
+        newCheckbox.config(text=filename)
+        newCheckbox.config(bg='#5a6465')
+        newCheckbox.config(activebackground='#5a6465')
+        newCheckbox.config(font=Font(family="Microsoft Sans Serif", size=14))
+        self.varList[filename] = tk.IntVar()
+        newCheckbox.config(var=self.varList[filename])
+        self.container.window_create(tk.INSERT, window=newCheckbox)
+        self.checkboxList.append(newCheckbox)
+
+    #gets a list of wanted files
+    def getSelected(self):
+
+        fileList = []
+
+        for key, value in self.varList.items():
+            if value.get():
+                fileList.append(key)
+
+        
+        print(fileList)
+        return fileList
+    
+    #mass modify functions
+    def selectAll(self):
+        for checkbox in self.checkboxList:
+            checkbox.select()
+
+    def deselectAll(self):
+        for checkbox in self.checkboxList:
+            checkbox.deselect()
 
 class Application:
 
@@ -10,11 +60,19 @@ class Application:
     probSelect = object()
     testBench = object()
     master = object()
+    terminalScroller = object()
+    testContainer = object()
+    outputSelector = object()
+    testControls = object()
+    selectionButtons = object()
 
     #fonts
     headerFont = object()
     textFont = object()
     buttonFont = object()
+
+    #test case manager
+    tcm = object()
 
     def __init__(self, master):
 
@@ -28,16 +86,22 @@ class Application:
         self.probSelect = self.builder.get_object('probSelect', self.master)
         self.testBench = self.builder.get_object('testBench', self.master)
         self.yearSelector = self.builder.get_object('yearSelector', self.master)
-
+        self.terminalScroller = self.builder.get_object('terminalScroller', self.master)
+        self.testContainer = self.builder.get_object('testContainer', self.master)
+        self.testControls = self.builder.get_object('testControls', self.master)
+        self.outputSelector = self.builder.get_object('outputSelector', self.master)
+        self.selectionButtons = self.builder.get_object('selectionButtons', self.master)
+        
         #mainwindow config
         #self.master.bind('<Configure>', self.resize)
-        self.master.minsize(809, 500)
+        self.master.minsize(1000, 600)
 
         #init and place fonts
         self.headerFont = Font(family="Microsoft Sans Serif", size=35)
-        self.textFont = Font(family="Microsoft Sans Serif", size=20)
+        self.textFont = Font(family="Microsoft Sans Serif", size=14)
         self.buttonFont = Font(family="Microsoft Sans Serif", size=27)
         self.yearSelector.config(font=self.textFont)
+        self.outputSelector.config(font=self.textFont)
 
         #configure expansion
         self.probSelect.grid(sticky=(tk.N, tk.S, tk.E, tk.W))
@@ -45,10 +109,26 @@ class Application:
         master.rowconfigure(0, weight=1)
         master.columnconfigure(0, weight=1)
 
+        self.terminalScroller.grid(sticky=(tk.N, tk.S, tk.E, tk.W))
+        self.testContainer.grid(sticky=(tk.N, tk.S, tk.E, tk.W))
+
         for i in self.probSelect.winfo_children():
             if isinstance(i, tk.Button):
                 i.config(font=self.buttonFont)
                 i.grid(sticky=(tk.N, tk.S, tk.E, tk.W))
+
+        for i in self.testBench.winfo_children():
+            if isinstance(i, tk.Button):
+                i.config(font=self.textFont)
+        for i in self.testControls.winfo_children():
+            if isinstance(i, tk.Button):
+                i.config(font=self.textFont)
+        for i in self.selectionButtons.winfo_children():
+            if isinstance(i, tk.Button):
+                i.config(font=self.textFont)
+
+
+        self.outputSelector.grid(sticky=(tk.E, tk.W))
         
         #show window
         self.showSelector()
@@ -64,7 +144,11 @@ class Application:
         'S3': lambda: self.selectionMade("S3"),
         'S4': lambda: self.selectionMade("S4"),
         'S5': lambda: self.selectionMade("S5"),
-        'back': lambda: self.showSelector()
+        'back': lambda: self.showSelector(),
+        'test': lambda: self.test(),
+        'testSel': lambda: self.testSel(),
+        'selectAll': lambda: self.tcm.selectAll(),
+        'deselectAll': lambda: self.tcm.deselectAll()
         }
         self.builder.connect_callbacks(callbacks)
 
@@ -80,7 +164,23 @@ class Application:
         self.testBench.tkraise() 
         yearSelected = self.yearSelector.get()
         questionSelected = question
-        self.master.title("CCCCCC - " +  str(yearSelected) + " " + question)
+        self.master.title("CCCCCC - " +  str(yearSelected) + " " + questionSelected)
+        fileList = glob.glob1('./data/' + str(yearSelected) + '/' + questionSelected + '/', '*.in')
+        nameList = []
+        
+        for filename in fileList:
+            name, _ = os.path.splitext(filename)
+            nameList.append(name)
+
+        self.tcm = testCaseManager(nameList, self.testContainer)
+        
+    
+    def test(self):
+        print("Testing All")
+    
+    def testSel(self):
+        print("Testing Sel")
+        self.tcm.getSelected()
 
 yearSelected = 2018
 questionSelected = "J1"
